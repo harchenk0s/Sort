4deeb83840e3fea5f68d467df30411e684d6ab74
HAROS <harchenk0s@yandex.ru>
2021-03-15 17:35:37 +0700

    Add Bubble Sort

---
+++ Sort.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+namespace Sort
+{
+    public class Sort
+    {
+        public int[] Bubble(int[] array)
+        {
+            int tmpElement;
+            bool isSorting = true;
+
+            while (isSorting)
+            {
+                isSorting = false;
+                for (int i = 0; i < array.Length-1; i++)
+                {
+                    if(array[i] > array[i + 1])
+                    {
+                        tmpElement = array[i + 1];
+                        array[i + 1] = array[i];
+                        array[i] = tmpElement;
+                        isSorting = true;
+                    }
+                }
+            }
+            return array;
+        }
+
+    }
+}
