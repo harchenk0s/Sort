8258b2a90c2fbae65dea3a9f31cd0bdef3d60830
HAROS <harchenk0s@yandex.ru>
2021-03-15 23:27:43 +0700

    Add Quick Sort

--- Sort.cs
+++ Sort.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace Sort
 {
@@ -11,11 +12,26 @@ namespace Sort
             v2 = tmpV;
         }
 
+        static int Partition(int[] array, int minIndex, int maxIndex)
+        {
+            int pivot = minIndex - 1;
+            for (int i = minIndex; i < maxIndex; i++)
+            {
+                if (array[i] < array[maxIndex])
+                {
+                    pivot++;
+                    Swap(ref array[pivot], ref array[i]);
+                }
+            }
+
+            pivot++;
+            Swap(ref array[pivot], ref array[maxIndex]);
+            return pivot;
+        }
+
+
         public int[] Bubble(int[] array)
         {
-            Console.WriteLine("Bubble Sort");
-            int arrayAccess = 0;
-            int actions = 0;
             bool isSorting = true;
 
             while (isSorting)
@@ -24,34 +40,26 @@ namespace Sort
 
                 for (int i = 0; i < array.Length-1; i++)
                 {
-                    arrayAccess++;
                     if(array[i] > array[i + 1])
                     {
-                        actions++;
                         Swap(ref array[i + 1], ref array[i]);
                         isSorting = true;
                     }
                 }
             }
-            Console.WriteLine($"Actions: {actions}, Accesses: {arrayAccess}");
             return array;
         }
 
         public int[] Shaker(int[] array)
         {
-            Console.WriteLine("Shaker Sort");
-            int actions = 0;
-            int accessions = 0;
             for (int i = 0; i < array.Length / 2; i++)
             {
                 bool isSwap = false;
 
                 for (int j = i; j < array.Length - i - 1; j++)
                 {
-                    accessions++;
                     if (array[j] > array[j + 1])
                     {
-                        actions++;
                         Swap(ref array[j], ref array[j + 1]);
                         isSwap = true;
                     }
@@ -59,10 +67,8 @@ namespace Sort
 
                 for (int j = array.Length - 2 - i; j > i; j--)
                 {
-                    accessions++;
                     if (array[j - 1] > array[j])
                     {
-                        actions++;
                         Swap(ref array[j - 1], ref array[j]);
                         isSwap = true;
                     }
@@ -70,12 +76,47 @@ namespace Sort
 
                 if (!isSwap)
                 {
-                    actions++;
                     break;
                 }
             }
-            Console.WriteLine($"Actions: {actions}, Accesses: {accessions}");
+            
             return array;
+        }
+
+        public int[] Insertion(int[] array)
+        {
+            for (int i = 1; i < array.Length; i++)
+            {
+                int key = array[i];
+                int j = i;
+                while ((j > 1) && (array[j - 1] > key))
+                {
+                    Swap(ref array[j - 1], ref array[j]);
+                    j--;
+                }
+
+                array[j] = key;
+            }
+            return array;
+        }
+
+        int[] QuickSort(int[] array, int minIndex, int maxIndex)
+        {
+            if (minIndex >= maxIndex)
+            {
+                return array;
+            }
+
+            int pivotIndex = Partition(array, minIndex, maxIndex);
+            QuickSort(array, minIndex, pivotIndex - 1);
+            QuickSort(array, pivotIndex + 1, maxIndex);
+
+            return array;
+        }
+
+        public int[] QuickSort(int[] array)
+        {
+            return QuickSort(array, 0, array.Length - 1);
         }
 
     }
